b75f555eefc8bdd8b9e6e9d7b50773cee6bb0eab
Urban Clem <arodriguez165@ucol.mx>
2024-03-11 14:41:34 -0500

    Arreglado resultado incorrecto

--- dist/index.js
+++ dist/index.js
@@ -1,22 +1,27 @@
-const fxo = document.getElementById("fxo");
-const fx1 = document.getElementById("fx1");
 const fx0 = document.getElementById("fx0");
+const fx1 = document.getElementById("fx1");
+const fx2 = document.getElementById("fx2");
+const x = document.getElementById("x");
 const x1 = document.getElementById("x1");
 const x0 = document.getElementById("x0");
 const x2 = document.getElementById("x2");
 const output = document.querySelector(".output");
 const submit = document.querySelector("#submit");
 
-function lnfxo() {
-  return Math.log(parseFloat(fxo.value));
+function lnfx0() {
+  return parseFloat(fx0.value);
 }
 
 function lnfx1() {
-  return Math.log(parseFloat(fx1.value));
+  return parseFloat(fx1.value);
 }
 
-function lnfx0() {
-  return Math.log(parseFloat(fx0.value));
+function lnfx2() {
+  return parseFloat(fx2.value);
+}
+
+function xcal() {
+  return parseFloat(x.value);
 }
 
 function x1cal() {
@@ -32,28 +37,20 @@ function x2cal() {
 }
 
 function quadraticInterpolation() {
-  const fxoVal = lnfxo();
-  const fx1Val = lnfx1();
-  const fx0Val = lnfx0();
-  const x1Val = x1cal();
-  const x0Val = x0cal();
-  const xVal = x2cal();
+  const fx0Val = parseFloat(fx0.value);
+  const fx1Val = parseFloat(fx1.value);
+  const fx2Val = parseFloat(fx2.value);
+  const xVal = parseFloat(x.value);
+  const x1Val = parseFloat(x1.value);
+  const x0Val = parseFloat(x0.value);
+  const x2Val = parseFloat(x2.value);
 
-  const numerator1 = (xVal - x0Val) * (xVal - x1Val);
-  const numerator2 = (xVal - x0Val) * (xVal - xoVal);
-  const numerator3 = (xVal - x1Val) * (xVal - xoVal);
+  const B1 = ((fx1Val - fx0Val) / (x1Val - x0Val));
+  const B2 = (((fx2Val - fx1Val) / (x2Val - x1Val) - B1) / (x2Val - x0Val));
 
-  const denominator1 = (x0Val - x1Val);
-  const denominator2 = (x0Val - xoVal);
-  const denominator3 = (x1Val - xoVal);
-
-  const result = (
-    (fxoVal * numerator1 / denominator1) +
-    (fx1Val * numerator2 / denominator2) +
-    (fx0Val * numerator3 / denominator3)
-  );
-
-  output.innerHTML = Math.exp(result);
+  const result = fx0Val + B1 * (xVal - x0Val) + B2 * (xVal - x0Val) * (xVal - x1Val);
+  
+  output.querySelector("#result").innerHTML = result;
 }
 
 submit.addEventListener("click", quadraticInterpolation);
